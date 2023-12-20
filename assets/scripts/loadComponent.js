c16169d9dfe16809d63583273f9028db32cd0396
oleg <oleg_zub00@mail.ru>
2023-12-20 18:48:47 +0300

    1 блок

--- assets/scripts/loadComponent.js
+++ assets/scripts/loadComponent.js
@@ -1,9 +1,13 @@
-export default function loadComponent (url, selector) {
+export default function loadComponent(url, selector) {
     const xhr = typeof XMLHttpRequest != 'undefined' ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     xhr.open('get', url, true);
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            document.querySelector(selector).innerHTML = xhr.responseText;
+            const components = document.querySelectorAll(selector)
+            for (let i = 0; i < components.length; i++) {
+                let htmlComp = components[i]
+                htmlComp.innerHTML = xhr.responseText
+            }
         }
     }
     xhr.send();
