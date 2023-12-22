e0462d1971eaf4e93205b5e1b96d2c7553ba54ce
oleg <oleg_zub00@mail.ru>
2023-12-22 15:12:23 +0300

    Анимации + визуал

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,10 @@
 import loadComponent from "./loadComponent.js"
 
 window.addEventListener('DOMContentLoaded', () => {
+    components()
+})
+
+function components() {
     loadComponent('./assets/components/header.html', '.header')
     loadComponent('./assets/components/footer.html', '.footer')
-})
+}
